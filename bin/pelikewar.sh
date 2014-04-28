03a8e9de8c88b8dc953c0d88795062b56a6d8f6e
Neel Smith <neel@pelike.hpcc.uh.edu>
2014-04-28 15:03:31 -0500

    Correct URLs on links.

---
+++ bin/pelikewar.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/home/neel/gradle/bin/gradle   -Pconf=/home/neel/repos/hmtdigital/confs/pelikeconf.gradle   -Plinks=/home/neel/repos/hmtdigital/confs/pelikelinks.gradle   -Pcustom=/home/neel/repos/hmtdigital/hmt/ war
+
