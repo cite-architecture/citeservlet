dc0b5b9fef05840d67c67e1b807e2de67e760b2c
Neel Smith <dnsmith.neel@gmail.com>
2014-06-03 09:27:45 -0400

    Remove local conf file.

--- bin/pelikewar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/home/neel/gradle/bin/gradle   -Pconf=/home/neel/repos/hmtdigital/confs/pelikeconf.gradle   -Plinks=/home/neel/repos/hmtdigital/confs/pelikelinks.gradle   -Pcustom=/home/neel/repos/hmtdigital/hmt/ war
-
